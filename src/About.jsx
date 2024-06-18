5ec8892079f719cb2d28a8812852754decb471e2
ahmedyassine benjeddou <ahmedyassinebenjeddou9@gmail.com>
2024-06-18 16:51:19 +0100

    f

---
+++ src/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import New from './New'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Compnent</h1>
+      <New/>
+    </div>
+  )
+}
+
+export default About
---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h1>Navbar</h1>
+      <ul>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, dolore repellat aperiam deleniti vero beatae itaque sequi commodi, tempore nesciunt eum? Libero sapiente cumque, corrupti impedit facilis dolore omnis ratione?</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
